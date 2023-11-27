5ae0e026d3bdabbb0ac8afba02ab54840aad2bfb
nicooduc <nykolasduchene@gmail.com>
2023-11-27 01:45:09 +0100

    Ajout des commentaires

--- src/app/fight/fight.component.ts
+++ src/app/fight/fight.component.ts
@@ -5,154 +5,166 @@ import {ActivatedRoute, Router} from "@angular/router";
 
 
 @Component({
-  selector: "epf-majors", templateUrl: "./fight.component.html", styleUrls: ["./fight.component.scss"],
+    selector: "epf-majors", templateUrl: "./fight.component.html", styleUrls: ["./fight.component.scss"],
 })
 export class FightComponent implements OnInit {
-  public xpGain: number = 0;
-  public monsterName: string | undefined;
-  public monsterImg: string | undefined;
-  public monsterHP: number | undefined;
-  public monsterAtk: number | undefined;
-  public monsterDef: number | undefined;
-  public monsterMag: number | undefined;
-  public monsterSpeed: number | undefined;
-  public heroName: string | undefined;
-  public heroImg: string | undefined;
-  public heroHP: number | undefined;
-  public heroAtk: number | undefined;
-  public heroDef: number | undefined;
-  public heroMag: number | undefined;
-  public heroSpeed: number | undefined;
-  public actionImg: string | undefined = "crusader.sprite.attack_banner.png";
-  private opponents: Opponent[] | undefined;
-  private monsterIndex: number | undefined;
-  private heroIndex: number | undefined;
-  private storyChoice: number | undefined;
+    // Déclaration des variables pour stocker les informations du combat
+    public xpGain: number = 0;
+    public monsterName: string | undefined;
+    public monsterImg: string | undefined;
+    public monsterHP: number | undefined;
+    public monsterAtk: number | undefined;
+    public monsterDef: number | undefined;
+    public monsterMag: number | undefined;
+    public monsterSpeed: number | undefined;
+    public heroName: string | undefined;
+    public heroImg: string | undefined;
+    public heroHP: number | undefined;
+    public heroAtk: number | undefined;
+    public heroDef: number | undefined;
+    public heroMag: number | undefined;
+    public heroSpeed: number | undefined;
+    public actionImg: string | undefined = "crusader.sprite.attack_banner.png";
+    // Variables privées
+    private opponents: Opponent[] | undefined;
+    private monsterIndex: number | undefined;
+    private heroIndex: number | undefined;
+    private storyChoice: number | undefined;
 
-  constructor(private fightService: FightService, private router: Router, private route: ActivatedRoute) {
-  }
-
-  ngOnInit(): void {
-    let idMonster: number = 1;
-    this.route.queryParams.subscribe(params => {
-      idMonster = params['idMonster'];
-      this.storyChoice = params['storyChoice'];
-      console.log(idMonster);
-      console.log(this.storyChoice);
-    });
-    this.fightService.startCombat(idMonster).subscribe((opponents: Opponent[]) => {
-      this.opponents = opponents;
-      this.updateOpponentCharacteristics();
-    });
-  }
-
-  public turn(action: String) {
-    switch (action) {
-        case "attack":
-          this.actionImg = "crusader.sprite.attack_sword.png";
-          break;
-        case "defend":
-          this.actionImg = "crusader.sprite.defend.png";
-          break;
-        case "castMagic":
-          this.actionImg = "crusader.sprite.attack_scroll.png";
-          break;
-    }
-    this.fightService.turn(action).subscribe((opponents: Opponent[]) => {
-      this.opponents = opponents;
-      this.updateOpponentCharacteristics();
-    })
-    this.checkMonsterStatus();
-    this.checkHeroStatus();
-  }
-
-  private updateOpponentCharacteristics(): void {
-    // Récupération de l'index de chaque opposant
-    this.monsterIndex = this.opponents?.findIndex(opponent => opponent.type === 'Monster');
-    this.heroIndex = this.opponents?.findIndex(opponent => opponent.type === 'Hero');
-
-    // Récupération des variables à afficher
-    if (this.opponents && this.monsterIndex !== undefined && this.heroIndex !== undefined) {
-      this.monsterName = this.opponents[this.monsterIndex]?.name;
-      this.monsterImg = this.opponents[this.monsterIndex]?.image;
-      console.log("monster image" + this.monsterImg);
-      this.monsterHP = this.opponents[this.monsterIndex]?.healthPoint;
-      this.monsterAtk = this.opponents[this.monsterIndex]?.attackPoint;
-      this.monsterDef = this.opponents[this.monsterIndex]?.defensePoint;
-      this.monsterMag = this.opponents[this.monsterIndex]?.magicPoint;
-      this.monsterSpeed = this.opponents[this.monsterIndex]?.speed;
-
-      this.heroName = this.opponents[this.heroIndex]?.name;
-      this.heroImg = this.opponents[this.heroIndex]?.image;
-      this.heroHP = this.opponents[this.heroIndex]?.healthPoint;
-      this.heroAtk = this.opponents[this.heroIndex]?.attackPoint;
-      this.heroDef = this.opponents[this.heroIndex]?.defensePoint;
-      this.heroMag = this.opponents[this.heroIndex]?.magicPoint;
-      this.heroSpeed = this.opponents[this.heroIndex]?.speed;
+    constructor(private fightService: FightService, private router: Router, private route: ActivatedRoute) {
     }
 
-  }
+    ngOnInit(): void {
+        // Initialisation du combat lors du chargement du composant
+        // Récupération des paramètres de l'URL
+        let idMonster: number = 1;
+        this.route.queryParams.subscribe(params => {
+            idMonster = params['idMonster'];
+            this.storyChoice = params['storyChoice'];
+            console.log(idMonster);
+            console.log(this.storyChoice);
+        });
+        // Appel au service pour commencer le combat
+        this.fightService.startCombat(idMonster).subscribe((opponents: Opponent[]) => {
+            this.opponents = opponents;
+            this.updateOpponentCharacteristics();
+        });
+    }
 
-  private checkMonsterStatus() {
-    this.fightService.checkOpponentStatus("Monster").subscribe((monsterState: boolean) => {
-      if (monsterState) {
-        this.monsterHP = 0;
-        this.fightEnd(true);
-      }
-    });
-  }
-
-  private checkHeroStatus() {
-    this.fightService.checkOpponentStatus("Hero").subscribe((heroState: boolean) => {
-      if (heroState) {
-        this.heroHP = 0;
-        this.fightEnd(false);
-      }
-    });
-  }
-
-  private fightEnd(success: boolean) {
-    this.fightService.endFight(success).subscribe((xpGain: number) => {
-      if (success) {
-        if (xpGain < 0) {
-          console.log("Level Up");
-          this.xpGain = Math.abs(xpGain);
-          const lvlUpMessage = document.getElementById('lvl-up-message');
-          const closeLvlUpMessage = document.getElementById('close-lvl-up-message');
-
-          if (lvlUpMessage && closeLvlUpMessage) {
-            lvlUpMessage.style.display = 'block';
-            closeLvlUpMessage.addEventListener('click', () => {
-              lvlUpMessage.style.display = 'none';
-              this.router.navigate(['/story'], {queryParams: {storyChoice: this.storyChoice}});
-            });
-          }
-        } else {
-          this.xpGain = xpGain;
-          const gainXPMessage = document.getElementById('gain-xp-message');
-          const closeGainXPMessage = document.getElementById('close-gain-xp-message');
-
-          if (gainXPMessage && closeGainXPMessage) {
-            gainXPMessage.style.display = 'block';
-            closeGainXPMessage.addEventListener('click', () => {
-              gainXPMessage.style.display = 'none';
-              this.router.navigate(['/story'], {queryParams: {storyChoice: this.storyChoice}});
-            });
-          }
+    // Méthode appelée lorsqu'un tour d'action est effectué
+    public turn(action: String) {
+        // Mise à jour de l'image d'action en fonction de l'action choisie
+        switch (action) {
+            case "attack":
+                this.actionImg = "crusader.sprite.attack_sword.png";
+                break;
+            case "defend":
+                this.actionImg = "crusader.sprite.defend.png";
+                break;
+            case "castMagic":
+                this.actionImg = "crusader.sprite.attack_scroll.png";
+                break;
         }
-        console.log(xpGain);
-      } else {
-        const deathMessage = document.getElementById('death-message');
-        const closedeathMessage = document.getElementById('close-death-message');
+        // Appel au service pour effectuer le tour d'action
+        this.fightService.turn(action).subscribe((opponents: Opponent[]) => {
+            this.opponents = opponents;
+            this.updateOpponentCharacteristics();
+        })
+        // Vérification de l'état du monstre et du héros après le tour
+        this.checkMonsterStatus();
+        this.checkHeroStatus();
+    }
 
-        if (deathMessage && closedeathMessage) {
-          deathMessage.style.display = 'block';
-          closedeathMessage.addEventListener('click', () => {
-            deathMessage.style.display = 'none';
-            this.router.navigate(['/story'], {queryParams: {storyChoice: 99}}); //TODO remplacer 99 par l'id de story de mort
-          });
+    // Méthode pour mettre à jour les caractéristiques des adversaires
+    private updateOpponentCharacteristics(): void {
+        // Récupération de l'index de chaque opposant
+        this.monsterIndex = this.opponents?.findIndex(opponent => opponent.type === 'Monster');
+        this.heroIndex = this.opponents?.findIndex(opponent => opponent.type === 'Hero');
+
+        // Récupération des variables à afficher
+        if (this.opponents && this.monsterIndex !== undefined && this.heroIndex !== undefined) {
+            this.monsterName = this.opponents[this.monsterIndex]?.name;
+            this.monsterImg = this.opponents[this.monsterIndex]?.image;
+            console.log("monster image" + this.monsterImg);
+            this.monsterHP = this.opponents[this.monsterIndex]?.healthPoint;
+            this.monsterAtk = this.opponents[this.monsterIndex]?.attackPoint;
+            this.monsterDef = this.opponents[this.monsterIndex]?.defensePoint;
+            this.monsterMag = this.opponents[this.monsterIndex]?.magicPoint;
+            this.monsterSpeed = this.opponents[this.monsterIndex]?.speed;
+
+            this.heroName = this.opponents[this.heroIndex]?.name;
+            this.heroImg = this.opponents[this.heroIndex]?.image;
+            this.heroHP = this.opponents[this.heroIndex]?.healthPoint;
+            this.heroAtk = this.opponents[this.heroIndex]?.attackPoint;
+            this.heroDef = this.opponents[this.heroIndex]?.defensePoint;
+            this.heroMag = this.opponents[this.heroIndex]?.magicPoint;
+            this.heroSpeed = this.opponents[this.heroIndex]?.speed;
         }
-      }
-    })
-  }
+
+    }
+
+    // Méthodes pour vérifier l'état du monstre et du héros
+    private checkMonsterStatus() {
+        this.fightService.checkOpponentStatus("Monster").subscribe((monsterState: boolean) => {
+            if (monsterState) {
+                this.monsterHP = 0;
+                this.fightEnd(true);
+            }
+        });
+    }
+
+    private checkHeroStatus() {
+        this.fightService.checkOpponentStatus("Hero").subscribe((heroState: boolean) => {
+            if (heroState) {
+                this.heroHP = 0;
+                this.fightEnd(false);
+            }
+        });
+    }
+
+    // Méthode appelée à la fin du combat
+    private fightEnd(success: boolean) {
+        this.fightService.endFight(success).subscribe((xpGain: number) => {
+            if (success) {
+                if (xpGain < 0) {
+                    console.log("Level Up");
+                    this.xpGain = Math.abs(xpGain);
+                    const lvlUpMessage = document.getElementById('lvl-up-message');
+                    const closeLvlUpMessage = document.getElementById('close-lvl-up-message');
+
+                    if (lvlUpMessage && closeLvlUpMessage) {
+                        lvlUpMessage.style.display = 'block';
+                        closeLvlUpMessage.addEventListener('click', () => {
+                            lvlUpMessage.style.display = 'none';
+                            this.router.navigate(['/story'], {queryParams: {storyChoice: this.storyChoice}});
+                        });
+                    }
+                } else {
+                    this.xpGain = xpGain;
+                    const gainXPMessage = document.getElementById('gain-xp-message');
+                    const closeGainXPMessage = document.getElementById('close-gain-xp-message');
+
+                    if (gainXPMessage && closeGainXPMessage) {
+                        gainXPMessage.style.display = 'block';
+                        closeGainXPMessage.addEventListener('click', () => {
+                            gainXPMessage.style.display = 'none';
+                            this.router.navigate(['/story'], {queryParams: {storyChoice: this.storyChoice}});
+                        });
+                    }
+                }
+                console.log(xpGain);
+            } else {
+                const deathMessage = document.getElementById('death-message');
+                const closedeathMessage = document.getElementById('close-death-message');
+
+                if (deathMessage && closedeathMessage) {
+                    deathMessage.style.display = 'block';
+                    closedeathMessage.addEventListener('click', () => {
+                        deathMessage.style.display = 'none';
+                        this.router.navigate(['/story'], {queryParams: {storyChoice: 99}}); //TODO remplacer 99 par l'id de story de mort
+                    });
+                }
+            }
+        })
+    }
 }
--- src/app/story/story.component.ts
+++ src/app/story/story.component.ts
@@ -1,79 +1,94 @@
-import {Component, OnInit} from "@angular/core"
+import {Component, OnInit} from "@angular/core";
 import {StoryService} from "services/story.service";
 import {Story} from "../models/story.model";
 import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-  selector: "epf-majors", templateUrl: "./story.component.html", styleUrls: ["./story.component.scss"],
+    selector: "epf-majors", templateUrl: "./story.component.html", styleUrls: ["./story.component.scss"],
 })
 export class StoryComponent implements OnInit {
-  public textDescription: string | undefined;
-  public localisation: string | undefined;
-  public option1: string | undefined;
-  public option2: string | undefined;
-  public option3: string | undefined;
-  public option4: string | undefined;
-  public storyImg: string | undefined;
+    // Variables pour stocker les détails de l'histoire
+    public textDescription: string | undefined;
+    public localisation: string | undefined;
+    public option1: string | undefined;
+    public option2: string | undefined;
+    public option3: string | undefined;
+    public option4: string | undefined;
+    public storyImg: string | undefined;
 
-  private story: Story | undefined;
-  private storyChoice: number | undefined;
+    // Variable pour stocker l'objet Story récupéré du service
+    private story: Story | undefined;
 
-  constructor(private storyService: StoryService, private router: Router, private route: ActivatedRoute) {
-  }
+    // Variable pour stocker le choix d'histoire
+    private storyChoice: number | undefined;
 
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.storyChoice = params['storyChoice'];
-      console.log("Appel route : " + this.storyChoice);
-    });
-    this.storyChoice = this.storyChoice ?? 1;
-    console.log("Final choice : " + this.storyChoice);
-    this.storyService.startStory(this.storyChoice).subscribe((story: Story) => {
-      this.story = story;
-      this.updateStory();
-    });
-  }
-
-  choice(choice: number) {
-    switch (choice) {
-      case 1:
-        this.storyChoice = this.story?.redirection1;
-        break;
-      case 2:
-        this.storyChoice = this.story?.redirection2;
-        break;
-      case 3:
-        this.storyChoice = this.story?.redirection3;
-        break;
-      case 4:
-        this.storyChoice = this.story?.redirection4;
-        break;
+    constructor(private storyService: StoryService, private router: Router, private route: ActivatedRoute) {
     }
-    if (this.storyChoice == undefined) {
-      console.error("Pas de choix possible");
-    } else if (this.storyChoice < 0) {
-      this.storyChoice = Math.abs(this.storyChoice);
-      this.router.navigate(['/fight'], {
-        queryParams: {
-          idMonster: this.story?.monsterID,
-          storyChoice: this.storyChoice
+
+    ngOnInit(): void {
+        // Récupération du choix d'histoire depuis les paramètres de l'URL
+        this.route.queryParams.subscribe((params) => {
+            this.storyChoice = params["storyChoice"];
+            console.log("Appel route : " + this.storyChoice);
+        });
+
+        // Si storyChoice est null, affecte une valeur par défaut de 1
+        this.storyChoice = this.storyChoice ?? 1;
+        console.log("Final choice : " + this.storyChoice);
+
+        // Appel du service pour démarrer l'histoire en fonction du choix
+        this.storyService.startStory(this.storyChoice).subscribe((story: Story) => {
+            this.story = story;
+            this.updateStory();
+        });
+    }
+
+    // Méthode appelée lorsqu'un choix est fait
+    choice(choice: number) {
+        // Switch pour déterminer le choix spécifique fait par l'utilisateur
+        switch (choice) {
+            case 1:
+                this.storyChoice = this.story?.redirection1;
+                break;
+            case 2:
+                this.storyChoice = this.story?.redirection2;
+                break;
+            case 3:
+                this.storyChoice = this.story?.redirection3;
+                break;
+            case 4:
+                this.storyChoice = this.story?.redirection4;
+                break;
         }
-      });
-    } else {
-      this.storyService.storyChoice(this.storyChoice).subscribe((story: Story) => {
-        this.story = story;
-        this.updateStory();
-      })
-    }
-  }
 
-  private updateStory() {
-    this.textDescription = this.story?.textDescription;
-    this.localisation = this.story?.localisation;
-    this.option1 = this.story?.option1;
-    this.option2 = this.story?.option2;
-    this.option3 = this.story?.option3;
-    this.option4 = this.story?.option4;
-    this.storyImg = this.story?.image;
-  }
+        // Gestion des cas spéciaux de choix
+        if (this.storyChoice == undefined) {
+            console.error("Pas de choix possible");
+        } else if (this.storyChoice < 0) {
+            // Redirection vers le combat en passant l'ID du monstre
+            this.storyChoice = Math.abs(this.storyChoice);
+            this.router.navigate(['/fight'], {
+                queryParams: {
+                    idMonster: this.story?.monsterID, storyChoice: this.storyChoice,
+                },
+            });
+        } else {
+            // Appel du service pour récupérer la suite de l'histoire
+            this.storyService.storyChoice(this.storyChoice).subscribe((story: Story) => {
+                this.story = story;
+                this.updateStory();
+            });
+        }
+    }
+
+    // Méthode privée pour mettre à jour les détails de l'histoire
+    private updateStory() {
+        this.textDescription = this.story?.textDescription;
+        this.localisation = this.story?.localisation;
+        this.option1 = this.story?.option1;
+        this.option2 = this.story?.option2;
+        this.option3 = this.story?.option3;
+        this.option4 = this.story?.option4;
+        this.storyImg = this.story?.image;
+    }
 }
